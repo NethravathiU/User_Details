d65bbf7c77ce9de32e4e2b09a179f0939981022f
Nethra <nethra.avni@gmail.com>
2024-12-08 22:59:56 +0100

    adding user

---
+++ main.py
@@ -0,0 +1,4 @@
+from user_info import user_details
+
+if __name__ == '__main__':
+    user_details()
---
+++ user_info.py
@@ -0,0 +1,9 @@
+def user_details():
+ firstname = (input("Enter your firstname: "))
+ lastname = (input("Enter your lastname: "))
+ age = int(input("enter your age: "))
+ Phonenumber = int(input("enter your number: "))
+ Jobtitle = (input("enter your Job-title: "))
+ print("first name: ", firstname, "last name:", lastname, "age:",age, "phonenumber:",Phonenumber, "jobtitle:", Jobtitle)
+
+ #return firstname,lastname, age, Phonenumber, Jobtitle
